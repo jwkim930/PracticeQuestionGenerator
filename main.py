5700689fc82ad4de245003e38327cbb3a7150d5b
jek710 <jek710@mail.usask.ca>
2025-11-18 14:57:05 -0600

    Added RationalExponentPractice
    * Four advanced question types,
      all work as intended.
    * Also fixed RadicalSimplify to return
      a list instead of Math.
    * Also fixed issue with FactorPolynomial
      where some problems showed exponents
      of zero.
    * Also added wrap attribute to
      UnsafeFraction.

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ from classes.environments import Multicols
 
 # Parameters (edit here)
 probs = [
-    [problems.WordProblem(100, '4cm', 'ShotGaugeDiameter', [(4, 20)])]
+    [problems.RationalExponentPractice(100, (-9, 9), (-9, 9), var=preset.variables)]
 ]
 prob_names = ["Test"]
-prob_insts = [preset.simplify_instruction]
+prob_insts = ["Rewrite the following expressions using powers with the least number of variables."]
 prob_cols = [1]
 # If mix_up is True, questions are generated in mixed order for that section.
 mix_up = [False]
-title = "Power Review"   # ignored if there's only one section
+title = "Midterm Review"   # ignored if there's only one section
 
 
 
