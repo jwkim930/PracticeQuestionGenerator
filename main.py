75b96343fda2cb6a65168be24d981fc9352c0f96
jek710 <jek710@mail.usask.ca>
2026-01-31 06:45:40 -0600

    Added word problems
    * Added super long word problems.
    * Also changed main.py to render
      word problems not in minipages.

--- main.py
+++ main.py
@@ -5,20 +5,31 @@ from pylatex import MiniPage
 from random import randint
 
 import classes.problems as problems
-import classes.problem_preset as preset
+# import classes.problem_preset as preset
 from classes.environments import Multicols
 
 
 # Parameters (edit here)
 probs = [
-    [problems.EquationMultiOperation(100, (-6, 6), 'double_frac_dist')]
-]
+    [problems.WordProblem(6, "10cm", "Grade11TrigSequencesQuadratics", [
+        (10000, 15000),
+        (5, 7),
+        (30, 35),
+        (1449, 1549),
+        (65, 75),
+        (16000, 20000),
+        (50, 60),
+        (400, 600),
+        (3500, 7000),
+        (5, 10)
+    ])]
+]  # outer list is section, inner list is problems within the section
 prob_names = ["Test"]   # name of each section
-prob_insts = [preset.equation_instruction]   # instruction for each section
+prob_insts = ["Testing"]   # instruction for each section
 prob_cols = [1]   # number of columns for each section
 # If mix_up is True, questions are generated in mixed order for that section.
 mix_up = [False]
-title = "Midterm Review"   # ignored if there's only one section
+title = "Test"   # ignored if there's only one section
 
 
 
@@ -44,12 +55,17 @@ def print_problems(i: int):
             prob = probs[i][probi]
             qnum = f"Q {i+1}.{q}" if nsec > 1 else f"Q{q}"
             with doc.create(Subsection(qnum, False)):
-                with doc.create(MiniPage()):
-                    for elem in prob.get_problem():
-                        if isinstance(elem, problems.DocInjector):
-                            elem.inject(doc)
-                        else:
-                            doc.append(elem)
+                if not isinstance(prob, problems.WordProblem):
+                    with doc.create(MiniPage()):
+                        for elem in prob.get_problem():
+                            if isinstance(elem, problems.DocInjector):
+                                elem.inject(doc)
+                            else:
+                                doc.append(elem)
+                        doc.append(VerticalSpace(prob.vspace))
+                else:
+                    # page overflow should happen with word problems
+                    doc.append(prob.get_problem()[0])
                     doc.append(VerticalSpace(prob.vspace))
             if prob.num_quest == 0:
                 probs[i].pop(probi)
